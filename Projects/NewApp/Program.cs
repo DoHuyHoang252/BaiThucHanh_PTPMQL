407c8e80427628e5866decfda8e1fd9e7c6cb997
DoHuyHoang252 <hoang252uioi@gmail.com>
2023-09-07 17:56:06 +0700

    Thuc hanh C#-4

--- Projects/NewApp/Program.cs
+++ Projects/NewApp/Program.cs
@@ -1,9 +1,12 @@
-﻿Console.WriteLine("DoHuyHoang-2021050781");
+﻿Console.WriteLine("Do Huy Hoang - 2021050781");
 Console.WriteLine("Nhap so thu nhat");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Nhap so thu hai");
 int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Hieu cua {0} va {1} la {2}",a,b,a-b);
-Console.WriteLine("Tich cua {0} va {1} la {2}",a,b,a*b);
-Console.WriteLine("Thuong cua {0} va {1} la {2}",a,b,a/b);
-Console.WriteLine("So du cua phep chia {0} cho {1} la {2}",a,b,a%b);
+if(a == b){
+    Console.WriteLine("{0} bang {1}", a, b);
+} else {
+    if(a > b) {
+    Console.WriteLine("{0} lon hon {1}", a, b);
+    } else Console.WriteLine("{0} nho hon {1}", a, b);
+}
