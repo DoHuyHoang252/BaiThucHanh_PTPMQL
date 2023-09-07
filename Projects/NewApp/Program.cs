b4d61838171cab81ef2e654aa2917772e16b76ea
DoHuyHoang252 <hoang252uioi@gmail.com>
2023-09-07 17:54:39 +0700

    Thuc Hanh C#-3

--- Projects/NewApp/Program.cs
+++ Projects/NewApp/Program.cs
@@ -1,3 +1,9 @@
-﻿const string hoTen = "Do Huy Hoang";
-const int mssv = 2021050781;
-Console.WriteLine("Sinh vien {0} co ma so sinh vien la {1}", hoTen, mssv);
+﻿Console.WriteLine("DoHuyHoang-2021050781");
+Console.WriteLine("Nhap so thu nhat");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Nhap so thu hai");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Hieu cua {0} va {1} la {2}",a,b,a-b);
+Console.WriteLine("Tich cua {0} va {1} la {2}",a,b,a*b);
+Console.WriteLine("Thuong cua {0} va {1} la {2}",a,b,a/b);
+Console.WriteLine("So du cua phep chia {0} cho {1} la {2}",a,b,a%b);
