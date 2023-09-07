5ab39dd8d94177591beb6fddeea78d6118ae4b5a
DoHuyHoang252 <hoang252uioi@gmail.com>
2023-09-07 17:46:25 +0700

    Thuc Hanh C#-1

--- Projects/NewApp/Program.cs
+++ Projects/NewApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+long b = 10;
+float c = 12.1F;
+double d = 10.10;
+decimal e = 11.1M;
+bool f = true;
+char g = 'r';
+string h = "Do Huy Hoang - 2021050781";
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+
+
+
+
