b967c3dd2cddb6e102620ba787c0f7dbaf21b6ce
DoHuyHoang252 <hoang252uioi@gmail.com>
2023-10-10 22:22:27 +0700

    Thuc Hanh MVC - GiaiPhuongTrinhBac1

--- Projects/FirstWebMVC/Controllers/DemoController.cs
+++ Projects/FirstWebMVC/Controllers/DemoController.cs
@@ -10,36 +10,17 @@ namespace FirstWebMVC.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Index(double heSoA, double heSoB, double heSoC){
-            string phuongtrinh = "Phuong trinh da nhap la: " + heSoA + "x^2 + " + heSoB + "x + " + heSoC + " = 0";
+        public IActionResult Index(double heSoA, double heSoB){
+            string phuongtrinh = "Phuong trinh da nhap la: " + heSoA + "x + " + heSoB + " = 0";
             string result;
-            double delta = heSoB * heSoB - 4 * heSoA * heSoC;
-            double x1, x2;
-            if (heSoA == 0)
-            {
-                if (heSoB == 0)
-                {
+            if(heSoA != 0 ){
+                result =  "x = " + (-heSoB/heSoA);
+            } else {
+                if(heSoB == 0){
+                    result = "Phuong trinh co vo so nghiem!";
+                } else {
                     result = "Phuong trinh vo nghiem!";
                 }
-                else
-                {
-                    result = "Phuong trinh co mot nghiem: x = " + (-heSoC / heSoB);
-                }
-            }
-            else if (delta > 0)
-            {
-                x1 = (double)((-heSoB + Math.Sqrt(delta)) / (2 * heSoA));
-                x2 = (double)((-heSoB - Math.Sqrt(delta)) / (2 * heSoA));
-                result = "Phuong trinh co 2 nghiem la: x1 = " + x1 + " va x2 = " + x2;
-            }
-            else if (delta == 0)
-            {
-                x1 = (-heSoB / (2 * heSoA));
-                result = "Phong trinh co nghiem kep: x1 = x2 = " + x1;
-            }
-            else
-            {
-                result = "Phuong trinh vo nghiem!";
             }
             ViewBag.debai = phuongtrinh;
             ViewBag.noti = result;
