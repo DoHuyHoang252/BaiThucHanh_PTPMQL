25bd2e99040d5ffec81282a9ca8044899106cc49
DoHuyHoang252 <hoang252uioi@gmail.com>
2023-10-26 21:24:07 +0700

    Thuc Hanh MVC - Slide 20

--- Projects/FirstWebMVC/Models/Employee.cs
+++
@@ -1,8 +0,0 @@
-namespace FirstWebMVC.Models
-{
-    public class Employee : Person
-    {
-        public string EmployeeID { get; set; }
-        public int Age { get; set; }
-    }
-}
--- Projects/FirstWebMVC/Models/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace FirstWebMVC.Models
-{
-    public class Person 
-    {
-        public string PersonID { get; set; }
-        public string FullName { get; set; }
-        public string Address { get; set; }
-    }
-}
